585522da225fe12fc3ed17fed7f917086d85440f
Carlos Vasconcelos <67444086+vasconcelospybr@users.noreply.github.com>
2020-06-29 08:33:53 -0300

    Add files via upload

---
+++ Desafio01.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Editor Spyder
+
+Este é um arquivo de script temporário.
+"""
+## QUAL A SAÍDA DO CÓDIGO?
+
+fruits = "7"
+fruits = fruits + "0"
+eggs = int (fruits) + 3
+print (float (eggs))
+
+"""
+A. 703
+B. 10.0
+C. 73.0
+D. 70.3
+"""
